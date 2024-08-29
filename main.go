3584b77cb47397c21770415250946331e9646936
eugene <eugene@localhost.localdomain>
2024-08-30 00:42:59 +0300

    Add authentication at socks5 proxy

--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 	"github.com/things-go/go-socks5"
 )
 
+type myCredentialStore struct {
+	user     string
+	password string
+}
+
+func (cs *myCredentialStore) Valid(user, password, userAddr string) bool {
+	return user == cs.user && password == cs.password
+}
+
 func main() {
-	// Create a SOCKS5 server
+
+	auth := socks5.UserPassAuthenticator{
+		Credentials: &myCredentialStore{user: "login", password: "password"},
+	}
+
 	server := socks5.NewServer(
 		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
+		socks5.WithAuthMethods([]socks5.Authenticator{auth}),
 	)
 
 	if err := server.ListenAndServe("tcp", ":10800"); err != nil {
